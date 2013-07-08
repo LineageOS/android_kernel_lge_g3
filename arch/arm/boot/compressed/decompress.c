cdbd4fd6cf7cd25de927f91dbdc6537109c6430c
Kyungsik Lee <kyungsik.lee@lge.com>
2013-07-08 16:01:45 -0700

    [SQUASH] lz4 support
    https://github.com/invisiblek/android_kernel_lge_g3/commits/cm-13.0_lz4
    
    Change-Id: Ibff21b60cabe0cff42f9fb87fd5b5e1ccc58713a

--- arch/arm/boot/compressed/decompress.c
+++ arch/arm/boot/compressed/decompress.c
@@ -52,5 +52,5 @@ extern void error(char *);
 
 int do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x))
 {
-	return decompress(input, len, NULL, NULL, output, NULL, error);
+	return __decompress(input, len, NULL, NULL, output, 0, NULL, error);
 }
