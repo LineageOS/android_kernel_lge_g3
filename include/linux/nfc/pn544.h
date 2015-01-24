05a40bd57c56bb9670104da372c82b7c20e9d48e
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-24 17:12:39 -0500

    nfc: switch ls990 to use pn544 NFC chip and add pn544 magic
    * The ls990 uses the pn544 chip instead of the pn547 chip that the
      rest of the G3s use, so set the correct driver in the kernel
    * Since the userspace expects pn544.h in order to build the HAL,
      the code in the header has to include an ifndef to use not use
      pn544 code (from pn544_lge.h) for the any pn547 chips. Otherwise,
      use pn544 for any other chips that call pn544.h
    * The pn544_lge driver includes an ifdef for CONFIG_LGE_NFC_PN547
      but that specific is never defined in any of the configs, it is
      always used as CONFIG_LGE_NFC_PN547_C2 so add an that to the if
      as well (less confusion for the build system)
    
    Change-Id: I51f0a7c4fafd4c384ad31d447512a323e7a882aa

--- include/linux/nfc/pn544.h
+++ include/linux/nfc/pn544.h
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright (C) 2010 NXP Semiconductors
  *
  * This program is free software; you can redistribute it and/or modify
@@ -16,11 +16,42 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
+#ifndef _PN544_LGE_H_
+#define _PN544_LGE_H_
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/list.h>
+#include <linux/i2c.h>
+#include <linux/irq.h>
+#include <linux/jiffies.h>
+#include <linux/uaccess.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/io.h>
+#include <linux/platform_device.h>
+#include <linux/gpio.h>
+#include <linux/miscdevice.h>
+#include <linux/spinlock.h>
 
-#ifndef _PN547_LGE_H_
-#define _PN547_LGE_H_
+#include <mach/board_lge.h>
 
 #define PN544_MAGIC	0xE9
+
+/* LGE_START seunghyun.kwak@lge.com 2013-10-15 NFC Bringup for B2*/
+#ifdef CONFIG_LGE_NFC_PN547	
+#define PN544_DRV_NAME      "pn547"    
+#else
+#define PN544_DRV_NAME      "pn544"    
+#endif
+/* LGE_END seunghyun.kwak@lge.com 2013-10-15 NFC Bringup for B2*/
+
+//#define NFC_GPIO_VEN	47		// byunggu       
+//#define NFC_GPIO_IRQ	59    	// byunggu       
+//#define NFC_GPIO_FIRM	48    	// byunggu       
+//#define NFC_I2C_SLAVE_ADDR 	0x28    // byunggu       
 
 /*
  * PN544 power control via ioctl
@@ -30,5 +61,56 @@
  */
 #define PN544_SET_PWR	_IOW(PN544_MAGIC, 0x01, unsigned int)
 
-#endif /* _PN547_LGE_H_ */
+/* Everything following this ifndef is not needed or used on PN547 chips
+ * so only use it if PN574 is NOT defined
+ */
+#if !defined(CONFIG_LGE_NFC_PN547) || !defined(CONFIG_LGE_NFC_PN547_C2)
 
+#define PN544_HW_REVISION _IOR(PN544_MAGIC, 0x02, unsigned int)
+
+struct pn544_i2c_platform_data {
+	unsigned int sda_gpio;
+	unsigned int scl_gpio;
+	unsigned int irq_gpio;
+	unsigned int ven_gpio;
+	unsigned int firm_gpio;
+};
+
+struct pn544_dev	{
+	wait_queue_head_t	read_wq;
+	struct mutex		read_mutex;
+	struct i2c_client	*client;
+	struct miscdevice	pn544_device;
+	unsigned int 		ven_gpio;
+	unsigned int 		firm_gpio;
+	unsigned int		irq_gpio;
+	// LGE_START byunggu.kang@lge.com 2014-02-21 Change IRQ Trigger Condition as Rising Edge
+	unsigned int 		count_irq;
+	// LGE_END byunggu.kang@lge.com 2014-02-21 Change IRQ Trigger Condition as Rising Edge
+	bool			irq_enabled;
+	spinlock_t		irq_enabled_lock;
+};
+
+struct pn544_gpio {
+	unsigned int 		sda_gpio;	// byunggu
+	unsigned int		scl_gpio;	// byunggu
+	unsigned int 		ven_gpio;
+	unsigned int 		firm_gpio;
+	unsigned int		irq_gpio;	
+};
+
+#define LGE_NFC_READ_IRQ_MODIFY//DY_TEST
+
+/* seokmin added for debugging */
+#define PN544_INTERRUPT_CMD	2
+#define PN544_READ_POLLING_CMD 3
+
+#if defined(CONFIG_LGE_NFC_DEBUG_MESSAGE)
+#define dprintk(fmt, args...) printk(fmt, ##args)
+#else
+#define dprintk(fmt, args...) do{ } while(0)
+#endif
+
+#endif /* _CONFIG_LGE_NFC_PN547_ || _CONFIG_LGE_NFC_PN547_C2_ */
+
+#endif /* _PN544_LGE_H_ */
