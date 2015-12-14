9eeb928a692672d4e48ec705e09a796648009b5d
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-14 14:38:00 -0600

    [WIP] Import LGD855 V30A sources
    Change-Id: I7ae1850779e5f33f22251eb6edb8f575ef29cac2

--- arch/arm/mach-msm/include/mach/msm_spi.h
+++ arch/arm/mach-msm/include/mach/msm_spi.h
@@ -41,4 +41,7 @@ struct msm_spi_platform_data {
 	u32  bam_consumer_pipe_index;
 	u32  bam_producer_pipe_index;
 	bool rt_priority;
+#ifdef CONFIG_MACH_LGE
+	bool do_not_create_sysfs_file;
+#endif
 };
